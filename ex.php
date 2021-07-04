5e56f5bfe357d06ccc21c17dc7d6b0d29ed835b7
Pasha-006 <81646317+Pasha-006@users.noreply.github.com>
2021-07-04 09:31:00 +0530

    Add files via upload

---
+++ ex.php
@@ -0,0 +1,9 @@
+<?php
+$to='6281705859@vtext.com';
+$from='shaikrahulpasha00@gmail.com';
+$message='this is message';
+$header='form : $form';
+mail($to,'',$message,$header);
+        
+        ?>
+
---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<div class="container">
+		Any Queries or Help 
+                <a href="https://t.me/Customeragent_Bot"> Click here</a>
+	
+	</div>
+</footer>
